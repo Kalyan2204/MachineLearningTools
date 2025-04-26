dacb6b251a0750a68a22f5724d257caed156d47a
Kalyan Goswami <iit.kalyan@gmail.com>
2025-04-25 20:50:30 -0400

    updated the checking

--- classification.py
+++ classification.py
@@ -46,7 +46,6 @@ class BuildClasificationAlgo():
             logger.info(f"Accuracy Score = {clasification_algo_disc[clasification_algo][0]}")
             #logger.info(f"Confusion Matrix = {clasification_algo_disc[clasification_algo][1]}") 
             #logger.info(f"Model Parameter = {clasification_algo_disc[clasification_algo][2]}") 
-            print("check")
 
     @clock('[{elapsed:0.8f}s] : {name}')    
     def call_clasification_method(self):
